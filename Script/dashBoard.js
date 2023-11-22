497548450ec6f040622ccfe334b10b4961746ba7
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-22 21:42:27 +0530

    login functionality done

--- Script/dashBoard.js
+++ Script/dashBoard.js
@@ -1,13 +1,13 @@
-let eventForm = document.getElementById("event-form");
-let imageUrl = document.getElementById("event-poster");
-let eventName = document.getElementById("event-name");
-let eventDescription = document.getElementById("event-description");
-let eventDate = document.getElementById("event-date");
-let eventCategory = document.getElementById("event-category");
-let eventPrice = document.getElementById("event-price");
+// let eventForm = document.getElementById("event-form");
+// let imageUrl = document.getElementById("event-poster");
+// let eventName = document.getElementById("event-name");
+// let eventDescription = document.getElementById("event-description");
+// let eventDate = document.getElementById("event-date");
+// let eventCategory = document.getElementById("event-category");
+// let eventPrice = document.getElementById("event-price");
 
 
-eventForm.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    console.log(imageUrl.value,eventCategory.value,eventDate.value,eventDescription.value,eventPrice.value,eventName.value)
-})
+// eventForm.addEventListener("submit",(e)=>{
+//     e.preventDefault();
+//     console.log(imageUrl.value,eventCategory.value,eventDate.value,eventDescription.value,eventPrice.value,eventName.value)
+// })
